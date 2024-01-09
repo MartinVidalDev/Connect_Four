727250111e7fef922da8371ca6ed2fd46b9aed69
vida0018 <martin.vidal@etudiant.univ-reims.fr>
2024-01-09 23:03:44 +0100

    Correction de toStringPlateau avec son test

--- some_tests.py
+++ some_tests.py
@@ -1,18 +1,14 @@
 from Model.Constantes import *
 from Model.Plateau import *
 from Model.Pion import *
-
-p = construirePlateau()
-print(p)
-pion = construirePion(const.JAUNE)
-line = placerPionPlateau(p, pion, 2)
-print("Placement d’un pion en colonne 2. Numéro de ligne :", line)
-print(p)
+from random import randint
+from random import choice
 
 # Essais sur les couleurs
 print("\x1B[43m \x1B[0m : carré jaune ")
 print("\x1B[41m \x1B[0m : carré rouge ")
 print("\x1B[41mA\x1B[0m : A sur fond rouge")
+print("\n")
 
 def toStringPlateau(plateau):
     """
@@ -25,7 +21,12 @@ def toStringPlateau(plateau):
 
     for i in range(len(plateau)):
         for j in range(len(plateau[i])):
-            res += "|" + " "
+            if plateau[i][j] is not None and plateau[i][j].get(const.COULEUR) == 1:
+                res += "|" + "\x1B[41m \x1B[0m"
+            elif plateau[i][j] is not None and plateau[i][j].get(const.COULEUR) == 0:
+                res += "|" + "\x1B[43m \x1B[0m"
+            else:
+                res += "|" + " "
         res += "|\n"
 
     for i in range(len(plateau[0]) * 2 + 1):
@@ -36,3 +37,9 @@ def toStringPlateau(plateau):
         res += " " + str(i)
 
     return res
+
+p = construirePlateau()
+for _ in range(20):
+ placerPionPlateau(p, construirePion(choice(const.COULEURS)),
+ randint(0, const.NB_COLUMNS - 1))
+print(toStringPlateau(p))
