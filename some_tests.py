fa3c2322015bddee945c15a1d0fbaf02bd1dea6d
vida0018 <martin.vidal@etudiant.univ-reims.fr>
2024-01-09 22:13:20 +0100

    toStringPlateau implémentée et fonctionnelle

--- some_tests.py
+++ some_tests.py
@@ -13,3 +13,26 @@ print(p)
 print("\x1B[43m \x1B[0m : carré jaune ")
 print("\x1B[41m \x1B[0m : carré rouge ")
 print("\x1B[41mA\x1B[0m : A sur fond rouge")
+
+def toStringPlateau(plateau):
+    """
+    Fonction permettant d'afficher un plateau de puissance 4 dans la console
+
+    :param plateau: Plateau que l'on veut transformer en chaîne de caractères (mode graphique)
+    :return: Retourne le plateau en chaîne de caractères
+    """
+    res = ""
+
+    for i in range(len(plateau)):
+        for j in range(len(plateau[i])):
+            res += "|" + " "
+        res += "|\n"
+
+    for i in range(len(plateau[0]) * 2 + 1):
+        res += "-"
+    res += "\n"
+
+    for i in range(len(plateau[0])):
+        res += " " + str(i)
+
+    return res
